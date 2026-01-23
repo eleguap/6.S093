d8fb2006ed0ee3e14621df593766b4b8c4b9b200
Andy Yu <andyyu132@gmail.com>
2026-01-23 03:57:37 -0500

    Add main.py

--- api/api.py
+++ api/api.py
@@ -166,7 +166,3 @@ async def get_feedback_stats():
     return {
         "result": "unimplemented"
     }
-
-# -------------------- Main --------------------
-if __name__ == "__main__":
-    uvicorn.run(app, host="0.0.0.0", port=8000)
